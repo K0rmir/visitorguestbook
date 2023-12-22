3e53b3519273c13ea94dfe8621a380d8dba38204
Edward Stewart <eddstewart1@hotmail.co.uk>
2023-12-22 14:01:05 +0000

    update api to render, update css, add media queries

--- client/app.js
+++ client/app.js
@@ -4,7 +4,7 @@
 const messageForm = document.getElementById("messageForm");
 
 async function getMessages() {
-    const response = await fetch("http://localhost:8080/messages");
+    const response = await fetch("https://visitor-guestbook-assignment.onrender.com");
     const messages = await response.json();
     for (let i = 0; i < messages.length; i++) {
         const username = messages[i].username;
@@ -24,7 +24,7 @@ messageForm.addEventListener("submit", async function(event) {
     event.preventDefault();
     const formData = new FormData(messageForm);
     const formValues = Object.fromEntries(formData);
-    const response = await fetch("http://localhost:8080/messages", {
+    const response = await fetch("https://visitor-guestbook-assignment.onrender.com", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
